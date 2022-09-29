9f2b404c6ec40d6f40d5a1345adf298ca41c4c0e
JUMURE <91860916+DohyeonY@users.noreply.github.com>
2022-09-29 21:31:46 +0900

    Update views.py

--- woopolemong_django/portfolios/views.py
+++ woopolemong_django/portfolios/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def detail(request) :
+    pass
+    return render(request, 'portfolios/detail.html',)
+
+def index(request) :
+    return render(request, 'portfolios/index.html',)
+
+def create(request) :
+    pass
+
+def delete(request) :
+    pass
+
+def update(request) :
+    pass
