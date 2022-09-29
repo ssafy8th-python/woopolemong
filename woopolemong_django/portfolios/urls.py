30972dbb9ea5ee0280592d5b8c4c1ba3694d7d37
appletail <csappletail@gmail.com>
2022-09-29 20:29:15 +0900

    name space

--- woopolemong_django/portfolios/urls.py
+++ woopolemong_django/portfolios/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+app_name = 'portfolios'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('portfolios/', include('portfolios.urls')),
+    
 ]
