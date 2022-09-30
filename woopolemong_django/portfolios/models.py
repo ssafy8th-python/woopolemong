5c50dc4c9b4ab02565bafc221f4b1f7e15f4a4b3
appletail <csappletail@gmail.com>
2022-09-30 13:22:43 +0900

    fix models

--- woopolemong_django/portfolios/models.py
+++ woopolemong_django/portfolios/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Portfolio(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.ForeignKey('accounts.User', related_name='portfolios', on_delete=)
+# class Portfolio(models.Model):
+#     title = models.CharField(max_length=100)
+#     content = models.TextField()
+#     author = models.ForeignKey('accounts.User', related_name='portfolios', on_delete=)
     
