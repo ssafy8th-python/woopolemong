94a2310c33bec2bd8104430fec910f991057fe35
appletail <csappletail@gmail.com>
2022-09-30 17:25:09 +0900

    portfolios models.py 제목 내용 작성자 카테고리 추가

--- woopolemong_django/portfolios/models.py
+++ woopolemong_django/portfolios/models.py
@@ -1,8 +1,12 @@
+from pyexpat import model
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
-# class Portfolio(models.Model):
-#     title = models.CharField(max_length=100)
-#     content = models.TextField()
-#     author = models.ForeignKey('accounts.User', related_name='portfolios', on_delete=)
-    
+class Portfolio(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey('accounts.User', related_name='portfolios', on_delete=models.SET_NULL, NULL=True)
+    category = models.CharField(max_lenth=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
