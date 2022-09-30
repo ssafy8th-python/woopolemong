80e600aa76fb1b5ee4db3cdfb3176001274c7c31
Lucas0105 <dnjswo105@gmail.com>
2022-09-30 17:33:49 +0900

    [feat]: login views 함수 구현

--- woopolemong_django/accounts/views.py
+++ woopolemong_django/accounts/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
 def login(request):
-    print('test')
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('portfolios:index')
+    elif request.method == 'GET':
+        form = AuthenticationForm()
+    
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'accounts/login.html', context)
+
 
 def logout():
     pass
