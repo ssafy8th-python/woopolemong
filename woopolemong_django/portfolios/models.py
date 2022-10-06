48714626007ef849ca3e951ac87b4103c2398094
appletail <csappletail@gmail.com>
2022-10-06 18:00:32 +0900

    portfolios model update

--- woopolemong_django/portfolios/models.py
+++ woopolemong_django/portfolios/models.py
@@ -1,12 +1,11 @@
-from pyexpat import model
-from unicodedata import category
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Portfolio(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey('accounts.User', related_name='portfolios', on_delete=models.SET_NULL, NULL=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, NULL=True)
     category = models.CharField(max_lenth=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
