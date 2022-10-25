2223de96659bae6768a7a35604dbfefa8f9bd72f
Lucas0105 <dnjswo105@gmail.com>
2022-10-26 08:52:53 +0900

    add 관리자페이지, 회원가입 시 로그인

--- woopolemong_django/accounts/views.py
+++ woopolemong_django/accounts/views.py
@@ -47,7 +47,8 @@ def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request, user)
 
             return redirect('portfolios:index')
 
